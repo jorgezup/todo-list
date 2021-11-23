8f51266e27e3526ae7b1156a30fca293f854bdd4
Jorge Zupirolli <jorgebertolo@gmail.com>
2021-11-23 12:50:10 -0800

    Realizado requisitos de 6 a 8 (obrigatorios)

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable editorconfig/editorconfig */
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable func-names */
 window.onload = function () {
   const input = document.getElementById('texto-tarefa');
   const btnCriarTarefa = document.getElementById('criar-tarefa');
   const olElement = document.getElementById('lista-tarefas');
-
+  
+  function changeBackgroundColor(e) {
+    const listOfLi = document.querySelectorAll('li');
+    for (let i = 0; i < listOfLi.length; i += 1) {
+      if (listOfLi[i].style.backgroundColor === 'rgb(128, 128, 128)') {
+        listOfLi[i].style.backgroundColor = 'rgb(255,255,255)';
+      }
+    }
+    e.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
   btnCriarTarefa.addEventListener('click', function () {
     const liElement = document.createElement('li');
     liElement.textContent = input.value;
     olElement.appendChild(liElement);
     input.value = '';
+    liElement.addEventListener('click', function () {
+      changeBackgroundColor(liElement);
+    });
   });
+
 };
